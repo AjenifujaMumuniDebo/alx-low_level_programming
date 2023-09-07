d116b3934f080a0a9fab0d4c80a8962f8a363151
AjenifujaMumuniDebo <daybour002@gmail.com>
2023-09-07 08:10:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+  
+  /*Using puts automatically adds a new line to the string output*/
+
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+
+}
