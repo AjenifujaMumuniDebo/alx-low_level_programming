6eb4306d508c946c3c2c7489c6d0a4b72cc8a9c9
AjenifujaMumuniDebo <daybour002@gmail.com>
2023-09-18 09:54:17 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * print_rev - prints strings in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int j;
+
+	for (j = _strlen(s) - 1; j >= 0; j--)
+	{
+		_putchar(*(s + j));
+	}
+	_putchar('\n');
+}
