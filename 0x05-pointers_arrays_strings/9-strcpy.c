084cd1edcd7fee21ee736684364412d0437cd1bf
AjenifujaMumuniDebo <daybour002@gmail.com>
2023-09-18 19:23:28 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,22 @@
 #include "main.h"
-#include "2-strlen.c"
 /**
- * _strcpy - main function to copy
- *
- * @dest: destination to copy
- * @src: src
- *
- * Return: a character value
+ * _strcpy -  copies the string pointed to by src.
+ * @src: array with the string source
+ * @dest: pointer where the test is paste
+ * Return: Always 0
  */
 char *_strcpy(char *dest, char *src)
 {
+
+	int index;
 	int i;
 
-	for (i = 0; i <= _strlen(src); i++)
+	for (i = 0; src[i] != 0; i++)
 	{
-		dest[i] = src[i];
+	}
+	for (index = 0; index <= i; index++)
+	{
+		dest[index] = src[index];
 	}
 	return (dest);
 }
