c0c9d1180ca5a37be0449533514994e9466903c7
AjenifujaMumuniDebo <daybour002@gmail.com>
2023-09-18 19:06:43 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include "2-strlen.c"
+
 /**
- * _puts - puts a string
- *
- * @str: string to print
+ * _puts - to print a string
+ * @str: pointer of type char
+ * Return: void
  */
+
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; *(str + i) != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		_putchar(*(str + i));
+		printf("%c", str[i]);
+		i++;
 	}
-	_putchar('\n');
+	printf("\n");
 }
