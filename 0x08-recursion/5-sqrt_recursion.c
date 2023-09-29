937d8382180f00958f542cebe84fac2711ce8f89
AjenifujaMumuniDebo <daybour002@gmail.com>
2023-09-28 17:08:13 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,35 @@
 #include "main.h"
 /**
- * _sqrt_recursion - estimates the square root of a n
- * @n : the number to be calculted for its square root
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to find the square root.
  *
- *return : the square root of n or (-1) if n does not have a natural square root
- *
+ * Return: The natural square root of n, or -1 if n has no root.
  */
-int find_sqrt(int n, int guess)
-{
-	if (guess * guess == n) /* Base case: found the square root */
-	{
-		return (guess);
-	}
-	else if (guess * guess > n) /* Error case: n does not have a natural square root */
-	{
-		return (-1);
-	}
-	else /* Recursive case: continue searching */
-	{
-		return (find_sqrt(n, guess + 1));
-	}
-}
-
 int _sqrt_recursion(int n)
 {
+	/* Base cases */
 	if (n < 0)
-	{
-		return (-1);
-	}
-	return (find_sqrt(n, 0));
-}	
+	return (-1);
+	if (n == 0 || n == 1)
+	return (n);
 
+	/* Recursive case */
+	return (_sqrt_helper(n, 1));
+}
+
+/**
+ * _sqrt_helper - function for calculating the square root recursively.
+ * @n: The number for which to find the square root.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root of n, or -1 if no root.
+ */
+int _sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+	return (guess);
+	else if (guess * guess > n)
+	return (-1); /* n does not have a natural square root */
+	else
+	return (_sqrt_helper(n, guess + 1));
+}
